2c4d81b75f28341f7c27b37c83531012ea4f5c72
Dryden <drydenellis@gmail.com>
2025-07-18 21:15:07 -0400

    Added Snow+Stone Blending, and More Tread Optimizations

--- Performance.gd
+++ Performance.gd
@@ -1,11 +1,12 @@
 extends Label
 
 var average_fps := 0
+var total_checks := 0
 var total_fps := 0
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var fps = Engine.get_frames_per_second()
 	total_fps += fps
-	var average_fps = total_fps/2.0
-	total_fps = average_fps
+	total_checks += 1
+	var average_fps = total_fps/total_checks
 	text = "FPS: " + str(fps) + "\n Average FPS: " + str(average_fps)
